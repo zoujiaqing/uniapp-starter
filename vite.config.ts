bbd151f57de320ddf11eb3c26b69c7750e82f7c7
gumpBOBO <ganlongbo2@sina.com>
2023-03-31 21:38:00 +0800

    格式化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 // 需要安装 @types/node
-import { resolve } from "path";
-import uni from "@dcloudio/vite-plugin-uni";
+import { resolve } from 'path'
+import uni from '@dcloudio/vite-plugin-uni'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +9,9 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: resolve(__dirname, "src"),
+        find: '@',
+        replacement: resolve(__dirname, 'src'),
       },
     ],
   },
-});
+})
